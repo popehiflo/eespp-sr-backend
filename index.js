3dd1570d3ba96843f5d7989d41696135c0aed75a
popehiflo <poolpetterhijuel@hotmail.com>
2022-07-14 22:17:15 -0500

    feat: install express and server up

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("EESPP SR Backend");
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('EESPP-SR Backend Server is running on port 3000');
+});
